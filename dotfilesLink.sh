af93df1ad38eaa7966b2262d3a8b9a0eb89d255a
koki_muguruma <koki_muguruma@forcia.com>
2020-04-14 02:03:47 +0900

    first add nvim_config

---
+++ dotfilesLink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/fish/fishfile ~/.config/fish/fishfile
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+
+# nvim参照: https://qiita.com/Ress/items/7e71e007cf8d41a07a1a
--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,12 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+# pyenv
+set PYENV_ROOT ~/.pyenv/bin
+set -x PATH $PYENV_ROOT $PATH
+status --is-interactive; and pyenv init - | source
+
+# nvim
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
