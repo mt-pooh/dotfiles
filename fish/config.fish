bd9e941cd4f57f2805414c9d22e99c5b3b5d3d16
koki_muguruma <koki_muguruma@forcia.com>
2020-04-16 11:54:25 +0900

    first add alias

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,12 @@ status --is-interactive; and pyenv init - | source
 # nvim
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
+
+# alias
+alias ..='cd ..'
+alias vi='nvim'
+alias gst='git status'
+alias gc='git commit'
+alias gd='git diff'
+alias gca='git commit --amend'
+alias glo='git log --oneline'
