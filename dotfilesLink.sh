e10fa198a4b067d879e8aee36eee7381f4b5499a
koki_muguruma <flash03stationery+git@gmail.com>
2020-04-16 12:35:46 +0900

    neo vim install追加（デフォルトコメントアウト）

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,23 @@ ln -sf ~/dotfiles/fish/fishfile ~/.config/fish/fishfile
 ln -sf ~/dotfiles/nvim ~/.config/nvim
 
 # nvim参照: https://qiita.com/Ress/items/7e71e007cf8d41a07a1a
+# nvim準備
+# pyenv　
+#git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+#sudo ~/.pyenv/plugins/python-build/install.sh
+#pyenv install 3.7.2
+#pyenv rehash
+#pyenv global 3.7.2
+#pyenv version
+#python --version
+#pip --version
+#pip install neovim
+#pip freeze | grep neovim
+#
+#source ~/.config/fish/config.fish
+#
+#sudo add-apt-repository ppa:neovim-ppa/stable
+#sudo apt-get update
+#sudo apt-get install neovim
+
+
